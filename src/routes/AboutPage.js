8076247c736e150804422d49a62fa61a70a0a176
kukielski <jasonkelske@gmail.com>
2025-07-07 14:11:42 -0500

    updated about page and gitignore .env

--- src/routes/AboutPage.js
+++ src/routes/AboutPage.js
@@ -4,8 +4,8 @@ export default function AboutPage() {
   return <div className="about-container">
     <h1>About</h1>
     <p className='about-description'>
-    This simple app returns a list of all lessons from all teams 
-    for a ConveYour client. To utilize it, you'll need the client's 
-    domain name and their Zapier API key.</p>
+    This simple app returns a list of all lessons, campaigns, and automations
+    from all teams for a ConveYour client. To utilize it, you'll need the client's 
+    domain name and Zapier API key.</p>
   </div>;
 }
